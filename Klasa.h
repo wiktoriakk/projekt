f9ea0434e6853cf7a6ba2b8725e5988d51f52c14
wiktoriakk <161590875+wiktoriakk@users.noreply.github.com>
2024-05-22 19:17:46 +0200

    Update Klasa.h

--- Klasa.h
+++ Klasa.h
@@ -1,9 +1,13 @@
 #pragma once
+#ifndef KLASA
+#define KLASA
 #include "API.h"
 #include <string>
 
 class EXPORT_API Klasa
 {
 public:
-	std::string getWeather(const std::string& city, const std::string& apiKey);
+    std::string getWeather(const std::string& city, const std::string& apiKey, bool isDaily); // Dodana nowa metoda z parametrem trybu prognozy
 };
+
+#endif // KLASA
